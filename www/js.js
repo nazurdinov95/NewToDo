5c24a24bdade63d079f2e935c34b5e6c0195682d
nazurdinov95 <56735406+nazurdinov95@users.noreply.github.com>
2019-10-30 00:23:42 +0500

    2

---
+++ www/js.js
@@ -0,0 +1,56 @@
+const addButton = document.querySelector('.addButton');
+var input = document.querySelector('.input');
+const container = document.querySelector('.container');
+
+class item {
+    constructor(itemName){
+       this.createDiv(itemName);
+    }
+    createDiv(itemName){
+        let input =createDiv ('.input');
+       input.value = itemName;
+       input.disabled = true;
+       input.classList.add('item_input');
+       input.type = "text";
+
+       let itemBox = document.createElement('div');
+       itemBox.classList.add('item');
+
+        let editButton = document.createElement('button');
+        editButton.innerHTML = "Edit";
+        editButton.classList.add('editButton');
+
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = "Remove";
+        removeButton.classList.add('removeButton');
+
+        container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(editButton);
+        itemBox.appendChild(removeButton);
+
+        editButton.addEventListener("click", () => this.edit(input));
+        removeButton.addEventListener("click", () => this.remove(itemBox));
+    }
+    edit(input){
+        input.disabled = !input.disabled;
+    }
+    remove(item){
+       container.removeChild(item);
+    }
+}
+
+function check() {
+    if (input.value !="") {
+        new item(input.value);
+        input.value
+    }
+}
+
+addButton.addEventListener('click', check);
+window.addEventListener('keydown',(e) => {
+    if(e.wich == 13){
+        check();
+    }
+})
